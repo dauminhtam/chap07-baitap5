8b7ec23746c4d2369c48ecd6ff05d2a43993de7e
dauminhtam <106529964+dauminhtam@users.noreply.github.com>
2022-06-06 11:24:30 +0700

    Update chap07-baitap5.c

--- chap07-baitap5.c
+++ chap07-baitap5.c
@@ -8,14 +8,14 @@ int main(int argc, char *argv[]) {
 	printf ("nhap vao diem cua sinh vien: ");
 	scanf ("%f", &diem);
 	
-	if (diem >=75) {printf ("Loai A");
-		}
-		else if (diem >=65) {printf ("Loai B");
-		}
-		else if (diem >=45) {printf ("Loai C");
-		}
-		else if (diem >=35) {printf ("Loai D");
-		}
+	if (diem >=75) printf ("Loai A");
+		
+	else if (diem >=65) printf ("Loai B");
+		
+	else if (diem >=45) printf ("Loai C");
+		
+	else if (diem >=35) printf ("Loai D");
+		
 	else printf ("Loai E");
 
 	return 0;
